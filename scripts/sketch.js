2f1d6e2b9516fe1a7b725e125da1f2a1bd255d74
xithiox <xithiox@gmail.com>
2017-07-15 14:33:47 -0400

    Set up basic website and sketch

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,11 +1,20 @@
+// Global variables
+var canvas;
+
+
 //////////////////////////////////
 //  p5.js built-in functions    //
 //////////////////////////////////
 
 function setup() {
-    ;
+    var w = document.getElementById("sketch-container").offsetWidth;
+    
+    console.log(w);
+
+    canvas = createCanvas(w, 400);
+    canvas.parent('sketch-container');
 }
 
 function draw() {
-    ;
+    background(255, 0, 200);
 }
